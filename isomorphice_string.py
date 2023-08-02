cd39bf6e6a8dc35792cf1627bbddfe0240d5094d
satvik2607-sharma <85659670+satvik2607-sharma@users.noreply.github.com>
2023-08-02 17:31:30 +0530

    Add files via upload

---
+++ isomorphice_string.py
@@ -0,0 +1,22 @@
+def isIsomorphic(s:str, t:str)->bool:
+    if len(s) != len(t):
+        return False
+    
+    
+    mapST,mapTS={},{}
+    
+    for i in range(len(s)):
+        
+        c1,c2=s[i],t[i]
+        
+        if ((c1 in mapST and mapST[c1]!= c2) or c2 in mapTS and mapTS[c2]!= c1):
+            return False
+        mapST[c1] = c2
+        mapTS[c2]=c1
+    return True
+
+s="foo"
+t="baz"
+
+ans=isIsomorphic(s,t)
+print(ans)
