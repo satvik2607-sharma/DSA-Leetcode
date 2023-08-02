d68a853f1888e1d3522679188b89f81c9f09d549
satvik2607-sharma <85659670+satvik2607-sharma@users.noreply.github.com>
2023-08-02 11:02:32 +0530

    Add files via upload

---
+++ largets_odd_number_in_string.py
@@ -0,0 +1,15 @@
+def largestOddNumber(num:str)-> str:
+    i=len(num)-1
+    
+    while i>=0:
+        if int(num[i])%2 !=0:
+            return num[:i+1]
+        i-=1
+    return ""
+
+num='52'
+
+ans=largestOddNumber(num)
+print (ans)
+
+
